19a060b237a6157262b03dd97d1fdbe7ab1a439e
hehejie <2823886877@qq.com>
2023-06-01 23:11:35 +0800

    fix: 增加配置文章

--- .dumirc.ts
+++ .dumirc.ts
@@ -6,6 +6,11 @@ export default defineConfig({
   themeConfig: {
     name: '码虫博物馆',
     logo: 'https://tos.wyjloveyl.com/project/upload/upload/png/1676625043581b355a861819cc6c69229a5cd9d6fahehejie.png',
+    footer: '码虫博物馆',
+    socialLinks: {
+      github: 'https://github.com/Hehejie1',
+    },
+    nav: [{ title: '运维', link: '/operations_and_maintenance' }],
   },
   favicons: ['https://tos.wyjloveyl.com/project/upload/upload/ico/logo.ico'],
   base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
